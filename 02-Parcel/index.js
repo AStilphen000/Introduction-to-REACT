cd0df30cb5edd072370707dbe0affc72e23b68c4
astilphen000 <astilphen000@west-mec.org>
2019-11-20 18:22:35 -0700

    Parcel
    Created an app using react

---
+++ 02-Parcel/index.js
@@ -0,0 +1,7 @@
+import React from 'react' 
+import ReactDOM from 'react-dom' 
+import App from './App';
+
+
+ReactDOM.render(<p>yea !</p>,document.getElementById('App'))
+// ReactDOM.render(<p>please !</p>,document.getElementById('sidebar'))
